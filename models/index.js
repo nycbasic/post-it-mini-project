41636b917f7be0499fe1d56ec5d938a797f6519e
Sam <samuel.poon@baruchmail.cuny.edu>
2021-05-24 21:23:20 -0400

    Delete index.js

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.set('debug', true);
-mongoose.connect(
-	'mongodb+srv://nycbasic:Cqxq1433@!@web-development-projects-b1s6x.mongodb.net/post-it?retryWrites=true', { useNewUrlParser: true,  useUnifiedTopology: true },
-);
-
-mongoose.Promise = Promise;
-
-module.exports.PostIt = require('./postit');
