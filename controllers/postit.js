5a4028632def7355165acfb812822720aefcd07b
Spoon <nycbasic@gmail.com>
2023-01-23 13:47:32 -0800

    updated all of route controllers to try/catch blocks with async/await

--- controllers/postit.js
+++ controllers/postit.js
@@ -2,10 +2,12 @@ const { PostIt } = require("../models");
 
 const getAllPostIt = async (req, res) => {
   try {
-    const postits = await PostIt.find();
-    return res.status(201).json(postits);
+    const postIts = await PostIt.find();
+    return res.status(201).json(postIts);
   } catch (err) {
-    return res.send(err);
+    return res.status(400).json({
+      error: err.message,
+    });
   }
 };
 
@@ -13,44 +15,61 @@ const createPostIt = async (req, res) => {
   const { title, content } = req.body;
   try {
     const newPostIt = await PostIt.create({ title, content });
-    return res.status(201).json(newPostIt);
+    if (newPostIt.created) {
+      return res.status(201).json(newPostIt);
+    }
   } catch (err) {
-    res.send(err);
+    return res.status(400).json({
+      error: err.message,
+    });
   }
 };
 
 const getPostIt = async (req, res) => {
+  const { postItId } = req.params;
   try {
-  } catch (err) {}
-  PostIt.findById(req.params.postItId)
-    .then(function (data) {
-      res.json(data);
-    })
-    .catch(function (err) {
-      res.send(err);
+    const postIt = await PostIt.findById(postItId);
+    if (postIt) {
+      return res.status(201).json(postIt);
+    }
+  } catch (err) {
+    return res.status(400).json({
+      err: err.message,
     });
+  }
 };
 
-const updatePostIt = function (req, res) {
-  PostIt.findOneAndUpdate({ _id: req.params.postItId }, req.body, {
-    new: true,
-  })
-    .then(function (data) {
-      res.json(data);
-    })
-    .catch(function (err) {
-      res.send(err);
-    });
+const updatePostIt = async (req, res) => {
+  const { postItId } = req.params;
+  const { title, content } = req.body;
+  const payload = {
+    title,
+    content,
+  };
+  try {
+    const updatedPost = await PostIt.findOneAndUpdate(
+      { _id: postItId },
+      payload,
+      {
+        new: false,
+      }
+    );
+    return res.status(201).json(updatedPost);
+  } catch (err) {
+    return res.send(err);
+  }
 };
 
-const deletePostIt = function (req, res) {
-  PostIt.remove({ _id: req.params.postItId })
-    .then(function () {
-      res.json({ message: "object deleted" });
-    })
-    .catch(function (err) {
-      res.send(err);
+const deletePostIt = async (req, res) => {
+  const { postItId } = req.params;
+  try {
+    const deletedPost = await PostIt.deleteOne({ _id: postItId });
+    return res.status(201).json({
+      message: deletedPost,
     });
+  } catch (err) {
+    return res.send(err);
+  }
 };
 
 module.exports = {
